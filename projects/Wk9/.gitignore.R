87f319dfe8905c6c74ade20fdbeab4f861c823cb
XBonham-Yang <107122564+XBonham-Yang@users.noreply.github.com>
2022-09-09 14:38:59 +0100

    wk9

---
+++ projects/Wk9/.gitignore.R
@@ -0,0 +1,7 @@
+australia_customers.csv  
+france_customers.csv      
+germany_customers.csv     
+new_zealand_customers.csv 
+spain_customers.csv       
+uk_customers.csv          
+usa_customers.csv  
